75bcc68d74f2e86ab30cdb57580b71e768611a55
Gerardo Miranda <GMyoDesarrollador23@gmail.com>
2020-07-02 19:47:42 -0400

    docs agragado

---
+++ docs/precache-manifest.3195b1311409914e0344eac150083db3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a27a909879814bebec81b0b3793add",
+    "url": "/index.html"
+  },
+  {
+    "revision": "276046edc1c7b9b25930",
+    "url": "/static/css/main.53000b07.chunk.css"
+  },
+  {
+    "revision": "813340125952adac502d",
+    "url": "/static/js/2.8f5b0e35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f5b0e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "276046edc1c7b9b25930",
+    "url": "/static/js/main.f45386bf.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
